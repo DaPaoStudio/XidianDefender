f310f35745255dff2411b945b668f77afe1aa6bf
DaPao <dapaostudio.foxmail.com>
2018-10-24 18:23:50 +0800

    ObjectPool

---
+++ Assets/Scripts/Framework/Pool/IReusable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IReusable
+{
+    //当取出时调用
+    void OnSpawn();
+
+    //当回收时调用
+    void OnUnspawn();
+}
