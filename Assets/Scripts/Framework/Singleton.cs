db0bd2b4afaf4651fbe46e6ccdc725400bd7921c
Dapao <Dapaostudio@foxmail.com>
2018-10-31 02:45:50 +0800

    Class Sound

--- Assets/Scripts/Framework/Singleton.cs
+++ Assets/Scripts/Framework/Singleton.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour 
-    where T : MonoBehaviour
+public abstract class Singleton<T> : MonoBehaviour //Singleton的泛型集合
+    where T : MonoBehaviour                        //对泛型进行类型约束：必须继承于MonoBehaviour
 {
     private static T m_instance = null;
 
