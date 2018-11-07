fb06aa99c18672468026dec218b53338412c1e8e
DaPao <dapaostudio.foxmail.com>
2018-11-07 21:54:38 +0800

    MapEditor

---
+++ Assets/Scripts/Application/Data/Point.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//格子坐标的索引
+public class Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+}
